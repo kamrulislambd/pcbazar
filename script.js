081d6cc37c15524225445395aa06f8a0f1319310
kamrulislambd <upwork.kamrul@gmail.com>
2021-09-18 22:57:34 +0600

    updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let menu = document.querySelector('#menubtn');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header__navbar');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
 }
 
 window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+menu.classList.remove('fa-times');
+navbar.classList.remove('active');
 }
